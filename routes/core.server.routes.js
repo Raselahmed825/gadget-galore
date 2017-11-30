d979b2cfc9852a9e961deac493ccfc5117caee6a
raselahmed825 <raselahmed825@gmail.com>
2017-11-30 08:32:26 +0000

    HelpPageLinked

--- routes/core.server.routes.js
+++ routes/core.server.routes.js
@@ -6,6 +6,8 @@ module.exports = function(app) {
 	app.route('/').get(core.index);
 	app.route('/about').get(core.about);
 	
+	app.route('/index').get(core.index);
+	app.route('/help').get(core.help);
 	app.route('/cart').get(core.cart);
 	//app.route('/products').get(core.products);
 	app.route('/confirmation').get(core.confirmation);
